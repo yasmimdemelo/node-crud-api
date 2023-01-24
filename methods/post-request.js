d08c7c1f98ebc1d93291cb5593b16ce18f243fe3
Yasmim <yasmimdmf95@gmail.com>
2023-01-24 17:02:06 -0300

    feat: add importation writeToFile in the post-request

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 const requestBodyparser = require("../util/body-parser");
+const writeToFile = require("../util/write-to-file");
 
 module.exports = async (req, res) => {
     if (req.url === "/api/movies") {
@@ -7,6 +8,7 @@ module.exports = async (req, res) => {
             let body = await requestBodyparser (req);
             body.id = crypto.randomUUID();
             req.movies.push(body);
+            writeToFile(req.movies);
             res.writeHead(201, { "Content-Type": "applicantion/json" });
             res.end();
         } catch (err) {
