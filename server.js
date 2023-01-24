44e4448efb4e699e1d3ca68abe6dcb7244599207
Yasmim <yasmimdmf95@gmail.com>
2023-01-23 22:52:09 -0300

    feat: add server http and port to listen

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 5001;
+
+const server = http.createServer((req, resp) => {
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
