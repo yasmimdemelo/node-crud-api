42b82d4961f6b7a4a670b884d63cf09e68624c15
Yasmim <yasmimdmf95@gmail.com>
2023-01-24 16:16:11 -0300

    feat: add promise, async and await in the req/res

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,8 +1,10 @@
+const crypto = require("crypto");
 const requestBodyparser = require("../util/body-parser");
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     if (req.url === "/api/movies") {
         try {
-            console.log("Request Body:", req.body)
+            let body = await requestBodyparser (req);
+            console.log("Request Body:", body)
         } catch(err) {}
     }
 };
--- util/body-parser.js
+++ util/body-parser.js
@@ -1,16 +1,16 @@
-module.exports = (request) => {
+module.exports = async (request) => {
     return new Promise((resolve, reject) => {
-        try {
-            let body = "";
-            request.on("data", (chunk) => {
-                body += chunk;
-            });
-            request.on("end", () => {
-                resolve(JSON.parse(body));
-            });
-        }catch(err) {
-            console.log(err);
-            reject(err);
-        }
+      try {
+        let body = "";
+        request.on("data", (chunk) => {
+          body += chunk;
+        });
+        request.on("end", () => {
+          resolve(JSON.parse(body));
+        });
+      } catch (err) {
+        console.log(err);
+        reject(err);
+      }
     });
-};
+  };
