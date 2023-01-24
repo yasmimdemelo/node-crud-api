4833574862e4237be6a07c0da08cd00de34ebfc1
Yasmim <yasmimdmf95@gmail.com>
2023-01-24 09:55:07 -0300

    feat: add switch case for get, post, put and delete request

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const http = require('http');
 const PORT = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
+    switch (req.method) {
+        case "GET":
+            getReq(req, res);
+            break;
+        case "POST":
+            postReq(req, res);
+            break;
+        case "PUT":
+            putReq(req, res);
+            break;
+        case "DELETE":
+            deleteReq(req, res);
+            break;
+    }
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.write(JSON.stringify({message: "Hello to Nodejs"}));
