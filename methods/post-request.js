34463ca3d9c472a8efa8a6dd9bac573660a222df
Yasmim <yasmimdmf95@gmail.com>
2023-01-24 16:32:01 -0300

    feat: add content in the post-request.js

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,10 +1,26 @@
 const crypto = require("crypto");
 const requestBodyparser = require("../util/body-parser");
+
 module.exports = async (req, res) => {
     if (req.url === "/api/movies") {
         try {
             let body = await requestBodyparser (req);
-            console.log("Request Body:", body)
-        } catch(err) {}
+            body.id = crypto.randomUUID();
+            req.movies.push(body);
+            res.writeHead(201, { "Content-Type": "applicantion/json" });
+            res.end();
+        } catch (err) {
+            console.log(err);
+            res.writeHead(400, { "Content-Type": "applicantion/json" });
+            res.end(
+                JSON.stringify({
+                    title: "Validation Failed",
+                    message: "Request body is not valid",
+                })
+            );
+        }
+    } else {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ title: "Not Found", message: "Route not found" }));
     }
 };
