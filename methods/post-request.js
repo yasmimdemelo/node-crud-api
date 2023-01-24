0b5e223ba5f91e30d0996a206901a7aa149056c7
Yasmim <yasmimdmf95@gmail.com>
2023-01-24 16:01:53 -0300

    feat: add event to data stream and import him to post-request.js

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,3 +1,4 @@
+const requestBodyparser = require("../util/body-parser");
 module.exports = (req, res) => {
     if (req.url === "/api/movies") {
         try {
--- util/body-parser.js
+++ util/body-parser.js
@@ -1,7 +1,13 @@
 module.exports = (request) => {
     return new Promise((resolve, reject) => {
         try {
-            
+            let body = "";
+            request.on("data", (chunk) => {
+                body += chunk;
+            });
+            request.on("end", () => {
+                resolve(JSON.parse(body));
+            });
         }catch(err) {
             console.log(err);
             reject(err);
